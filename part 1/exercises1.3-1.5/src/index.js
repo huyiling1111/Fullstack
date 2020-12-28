165280cd4ffbf59af2eca745fddf9e5a9695bf7d
huyiling1111 <1052859577@qq.com>
2020-12-28 14:06:08 +0800

    1.3

---
+++ part 1/exercises1.3-1.5/src/index.js
@@ -0,0 +1,31 @@
+import {React,useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  )
+}
+
+const rootElement=document.getElementById('root')
+ReactDOM.render(<App />,rootElement)
